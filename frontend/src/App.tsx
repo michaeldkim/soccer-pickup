25603de3e3d2059c427c1522c01db9e5513b5860
Michael <michaeldkim1@gmail.com>
2024-03-27 18:43:54 -0400

    Added more pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navigation } from './components';
-import { HomePage, AboutPage, LoginPage, LeaguesPage } from './pages';
+import { HomePage, AboutPage, LoginPage, LeaguesPage, RegisterPage, NotFound } from './pages';
 
 import './App.css';
 
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,11 +2,15 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import LoginPage from "./LoginPage";
 import LeaguesPage from "./LeaguesPage";
+import RegisterPage from "./RegisterPage";
+import NotFound from "./NotFound";
 
 export {
     HomePage,
     AboutPage,
     LoginPage,
     LeaguesPage,
+    RegisterPage,
+    NotFound,
 };
 
